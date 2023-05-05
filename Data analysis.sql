0f06d724eb3cccd66e21130b4fccc2736d322094
Harsh Srivastava <srivastava.harsh19@yahoo.com>
2023-05-05 23:27:09 +0530

    Adding a stored procedure to calculate the profit percentage

--- Data analysis.sql
+++ Data analysis.sql
@@ -30,7 +30,8 @@ inner join E_Commerce..OrderDetails od
 on ol.Order_ID= od.Order_ID
 group by ol.order_id,ol.CustomerName
 
-select * from E_commerce..#Temp_CustAmountSpent
+select CustomerName
+from E_commerce..#Temp_CustAmountSpent
 
 
 
@@ -58,7 +59,8 @@ inner join E_Commerce..OrderDetails od
 on ol.Order_ID= od.Order_ID
 group by ol.order_id,ol.CustomerName
 
-select * from E_commerce..#Temp_ProfitperCust
+select * 
+from E_commerce..#Temp_ProfitperCust
 
 
 
@@ -76,9 +78,37 @@ select @totalProfit
 
 
 
--- Profit percentage
+-- Finding out the Profit percentage
 
+create procedure ProfitPercentage
+as
+
+declare @totalProfit as float
+declare @totalSpent as float
 declare @profitPercentage as float
+
+set @totalSpent= (select sum([Total amount spent])
+from #Temp_CustAmountSpent)
+
+set @totalProfit = (select sum([Profit per customer])
+from #Temp_ProfitperCust)
+
+set @profitPercentage= (@totalProfit/@totalSpent)*100
+
+print 'Your Total Profit percentage is ' + cast(@profitPercentage as varchar) + '%'
+
+
+
+Execute ProfitPercentage
+
+
+-- If you want to drop the stored procedure execute the following 
+-- Drop Procedure ProfitPercentage
+
+
+
+
+
 
 
 
